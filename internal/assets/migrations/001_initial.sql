4aa2bae1ae17e3e3e6c277a7e2c8ebfacec739ed
dl <dl@MacBook-Pro-dl.local>
2024-10-30 13:30:08 +0200

    SQL migrations file

--- internal/assets/migrations/001_initial.sql
+++ internal/assets/migrations/001_initial.sql
@@ -1,3 +1,11 @@
 -- +migrate Up
 
+create table transfers(
+    tx_hash text primary key,
+    sender text not null,
+    receiver text not null,
+    amount decimal not null,
+);
+
 -- +migrate Down
+drop table transfers
