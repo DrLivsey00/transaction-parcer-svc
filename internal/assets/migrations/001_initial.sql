af67a8d9db151387276d6a66fa26e86c87a5b448
DrLivsey00 <emihrin50@gmail.com>
2024-10-31 11:27:43 +0200

    index added

--- internal/assets/migrations/001_initial.sql
+++ internal/assets/migrations/001_initial.sql
@@ -4,8 +4,11 @@ create table transfers(
     tx_hash text primary key,
     sender text not null,
     receiver text not null,
-    amount decimal not null,
+    token_amount decimal not null,
 );
+
+create index receiver_index on transfers(receiver);
+create index sender_index on transfers(sender)
 
 -- +migrate Down
 drop table transfers
